25b46b6e8f7d36d9541c2de0b1bd7833cb21d9f0
PanZaifeng <panzf1999@gmail.com>
2023-09-21 15:52:47 +0800

    copy results to host

--- AE/run_all.sh
+++ AE/run_all.sh
@@ -10,8 +10,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
 docker build -t recom:latest .
 docker run -d --gpus all --net=host --name recom_ae -it recom:latest
 docker exec recom_ae bash -c "git clone --branch features/dev_pan --recurse-submodules https://github.com/PanZaifeng/recom.git recom && python recom/AE/build_and_run.py"
+docker cp recom_ae:recom/AE/latency.pdf .
+docker cp recom_ae:recom/AE/throughput.pdf .
